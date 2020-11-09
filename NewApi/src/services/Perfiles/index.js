8235bdbd193cfd021692760d9232403d7943941a
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-11-08 23:52:39 -0500

    commit

--- NewApi/src/services/Perfiles/index.js
+++ NewApi/src/services/Perfiles/index.js
@@ -12,6 +12,6 @@ const server = new ApolloServer({
     ]),
 });
 
-server.listen({ port: 5002 }).then(({ url }) => {
+server.listen({ port: 2007 }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
