402d40c21ab004a983038a68a493c4fb454587e0
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-11-10 00:15:55 -0500

    gateway

--- NewApi/src/gateway.js
+++ NewApi/src/gateway.js
@@ -13,14 +13,13 @@ const config = { ssl: true, port: 5000, hostname: "localhost" };
 
 const gateway = new ApolloGateway({
     serviceList: [
-
-        //{ name: "Exams", url: `http://localhost:2001` },
-        //{ name: "Vocabulary", url: `http://localhost:2002` },
-        //{ name: "Levels", url: `http://localhost:2003` },
+        { name: "Exams", url: `http://localhost:2001` },
+        { name: "Vocabulary", url: `http://localhost:2002` },
+        { name: "Levels", url: `http://localhost:2003` },
         { name: "Login", url: `http://localhost:2004` },
-        //{ name: "Foro", url: `http://localhost:2005` },
-        //{ name: "Soporte", url: `http://localhost:2006` },
-        //{ name: "Perfiles", url: `http://localhost:2007` },
+        { name: "Foro", url: `http://localhost:2005` },
+        { name: "Soporte", url: `http://localhost:2006` },
+        { name: "Perfiles", url: `http://localhost:2007` },
     ], //Aqui se agregan los microcervicios con puertos diferentes 5001, 5002, 5003... y se crea su respectiva carpeta en services
     __exposeQueryPlanExperimental: true,
 });
