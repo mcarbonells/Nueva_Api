862343bb70e53a8c4b59906964d7942b04659497
RENE <alrramirezso@unal.edu.co>
2020-12-03 21:53:43 -0500

    perfiles vocabulario y examnes en cluster

--- src/services/Vocabulary/resolvers.js
+++ src/services/Vocabulary/resolvers.js
@@ -3,8 +3,8 @@ const dotenv = require ('dotenv') ;
 
 dotenv.config();
 
-const urlC = `http://54.224.42.223:3001/category`;
-const urlW = `http://54.224.42.223:3001/words`;
+const urlC = `http://54.163.16.166:3001/category`;
+const urlW = `http://54.163.16.166:3001/words`;
 
 const resolvers = {
     Query: {
