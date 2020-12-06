9ed38496c558ed4127e8e5d68cab9e7597fa408f
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-12-06 16:59:16 -0500

    intento ?

--- src/gateway.js
+++ src/gateway.js
@@ -4,23 +4,24 @@ const express = require('express') ;
 const dotenv = require('dotenv');
 const fs = require('fs');
 const https = require('https');
+const http = require('http');
 
 dotenv.config();
 
 const uri = process.env.URI;
 
-const config = { ssl: true, port: 5000, hostname: "localhost" };
+const config = { ssl: false, port: 5000, hostname: "localhost" };
 
 const gateway = new ApolloGateway({
     serviceList: [
-        //{ name: "Exams", url: `http://localhost:2001` },
-        //{ name: "Vocabulary", url: `http://localhost:2002` },
-        //{ name: "Levels", url: `http://localhost:2003` },
-        //{ name: "Login", url: `http://localhost:2004` },
-        //{ name: "Foro", url: `http://localhost:2005` },
-        //{ name: "Soporte", url: `http://localhost:2006` },
+        { name: "Exams", url: `http://localhost:2001` },
+        { name: "Vocabulary", url: `http://localhost:2002` },
+        { name: "Levels", url: `http://localhost:2003` },
+        { name: "Login", url: `http://localhost:2004` },
+        { name: "Foro", url: `http://localhost:2005` },
+        { name: "Soporte", url: `http://localhost:2006` },
         { name: "Perfiles", url: `http://localhost:2007` },
-        //{ name: "Ss_1a", url: `http://localhost:2008` },
+        { name: "Ss_1a", url: `http://localhost:2008` },
     ], 
     __exposeQueryPlanExperimental: true,
 });
@@ -36,11 +37,11 @@ const apollo = new ApolloServer({
 const app = express();
 apollo.applyMiddleware({ app });
 
-let server = (https.createServer(
-    {
+let server = (http.createServer(
+    /*{
         key: fs.readFileSync(`./localhost.key`),
         cert: fs.readFileSync(`./localhost.crt`),
-    },
+    },*/
     app
 ));
 
